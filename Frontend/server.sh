352d3e0b23336988054839f13089bf9a491b7af7
juan <juankmiloh@hotmail.com>
2022-03-21 22:42:08 -0500

    Update archivo server.sh

--- Frontend/server.sh
+++ Frontend/server.sh
@@ -3,4 +3,4 @@
 # -- COMANDOS PARA LEVANTAR SERVIDOR
 source ~/.bashrc # Comando para actualizar la configuracion de nvm
 nvm use v14.18.2 # Comando para indicar la version de node a utilizar
-ng serve --port 5059
+ng serve --host 0.0.0.0 --port 5059 --disableHostCheck true
