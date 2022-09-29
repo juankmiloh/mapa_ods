cb3bf7d3d60843d8477290329aae70d018d6dae2
juan <juankmiloh@hotmail.com>
2022-09-29 12:14:56 -0500

    Cargue de certificados SSL

--- Backend/server.sh
+++ Backend/server.sh
@@ -2,3 +2,5 @@
 # echo levantar servidor python
 source venv/bin/activate
 flask run -h 0.0.0.0 -p 5059
+# flask run -h 0.0.0.0 -p 5000 --cert=superservicios.gov.co.crt --key=superservicios.key
+# PEM pass=%Abcs011549123.%
